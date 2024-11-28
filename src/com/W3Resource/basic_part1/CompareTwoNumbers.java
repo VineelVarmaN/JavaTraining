45954ac79522c969ee5046a26d0fb3805edf6704
VineelVarma N <vineelnemmani1training@gmail.com>
2024-11-28 22:28:08 +0530

    added W3Schools 28dec exercises

---
+++ src/com/W3Resource/basic_part1/CompareTwoNumbers.java
@@ -0,0 +1,13 @@
+package com.W3Resource.basic_part1;
+
+public class CompareTwoNumbers {
+    public static void main(String[] args) {
+        int n1 =25;
+        int n2 = 39;
+        if(n1 != n2 && n1<n2 && n1<=n2){
+            System.out.println(n1+" != "+n2);
+            System.out.println(n1+" < "+n2);
+            System.out.println(n1+" <= "+n2);
+        }
+    }
+}
---
+++ src/com/W3Resource/basic_part1/ConvertSecondsToHours.java
@@ -0,0 +1,12 @@
+package com.W3Resource.basic_part1;
+
+public class ConvertSecondsToHours {
+    public static void main(String[] args) {
+        int totalSeconds = 86399;
+        //convert seconds to hours, minutes and seconds by division
+        int hours = totalSeconds / 3600;
+        int minutes = (totalSeconds % 3600)/60;
+        int seconds = totalSeconds % 60;
+        System.out.println(hours + ":" + minutes + ":" + seconds);
+    }
+}
