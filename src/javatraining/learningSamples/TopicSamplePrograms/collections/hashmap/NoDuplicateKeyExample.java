c88e19a0f0131f4f38d965714aa98e4b4813634f
VineelVarma N <vineelnemmani1training@gmail.com>
2024-12-20 23:36:18 +0530

    added new java learning hash map examples

--- src/javatraining/learningSamples/TopicSamplePrograms/collections/hashmap/NoDuplicateKeyExample.java
+++ src/javatraining/learningSamples/TopicSamplePrograms/collections/hashmap/NoDuplicateKeyExample.java
@@ -3,15 +3,18 @@ package javatraining.learningSamples.TopicSamplePrograms.collections.hashmap;
 import java.util.HashMap;
 import java.util.Map;
 
-public class HashMapExamples {
+public class NoDuplicateKeyExample {
     public static void main(String[] args) {
         HashMap<Integer,String> map = new HashMap<>();
         map.put(1,"Vineel");
         map.put(2,"Varma");
+        map.put(3,"Nemmani");
         System.out.println(map);
-        //iterate and print the values
-        for (Map.Entry<Integer,String> names : map.entrySet()){
-            System.out.println(names.getKey()+" : "+names.getValue());
+        //iterating the values
+        for (Map.Entry<Integer,String> name: map.entrySet()){
+            System.out.println(name.getKey()+" : "+name.getValue());
         }
+        map.put(3,"Swathi");
+        System.out.println(map);
     }
 }
