49b1aa0c47618b93fe1e7b2e19ef816efdb8b1b6
VineelVarma N <vineelnemmani1training@gmail.com>
2025-01-21 00:21:18 +0530

    added W3Resource basic part1 examples

---
+++ src/javapractice/W3Resource/basic_part1/FirstAndLastElementSame.java
@@ -0,0 +1,25 @@
+package javapractice.W3Resource.basic_part1;
+
+import java.util.stream.IntStream;
+
+public class FirstAndLastElementSame {
+    public boolean getFirstAndLastElement(int[] array1){
+        //Check the length must be greater than or equal to 2
+        //check the first and last element of the array is same
+        if (array1.length>=2 && array1[0]==array1[array1.length-1]){
+                return true;
+        }
+        return false;
+    }
+    public boolean getFirstAndLastElementUsingStream(int[] array1){
+        boolean result = IntStream.of(array1.length).allMatch(length->length>=2 && array1[0]==array1[array1.length-1]);
+        return result;
+    }
+    public static void main(String[] args) {
+        int[] array1 = {50, -20, 0, 30, 40, 60, 50};
+        FirstAndLastElementSame objFirstAndLastElementSame = new FirstAndLastElementSame();
+        System.out.println(objFirstAndLastElementSame.getFirstAndLastElement(array1));
+        //using stream
+        System.out.println("Using stream : "+objFirstAndLastElementSame.getFirstAndLastElementUsingStream(array1));
+    }
+}
