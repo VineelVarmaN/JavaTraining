5b66761daf0ae28bbd63f99a74ee533197767ffd
VineelVarma N <vineelnemmani1training@gmail.com>
2025-02-17 23:35:52 +0530

    added W3Resource basic part1 examples on 17022025

---
+++ src/javapractice/W3Resource/basic_part1/SumDigitsToSingleDigit.java
@@ -0,0 +1,25 @@
+package javapractice.W3Resource.basic_part1;
+
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class SumDigitsToSingleDigit {
+    public static long getSumDigitsToSingleDigit( int number){
+        long sumDigit = 0;
+        //convert the number to string
+        String numberString = Integer.toString(number);
+        for (int i = 0; i<numberString.length(); i++){
+            sumDigit += Character.getNumericValue(numberString.charAt(i));
+        }
+        return sumDigit;
+    }
+    public static long getSumDigitsToSingleDigitUsingStream(int number){
+        String numberString = Integer.toString(number);
+        long result = numberString.chars().map(Character::getNumericValue).sum();
+        return result;
+    }
+    public static void main(String[] args) {
+        int number = 12345;
+        System.out.println(getSumDigitsToSingleDigit(number));
+    }
+}
