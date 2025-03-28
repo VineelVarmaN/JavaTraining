0d2dd9ecacd1a7b8ef5745ef4b1dd065abe5416d
VineelVarma N <vineelnemmani1training@gmail.com>
2025-03-28 21:14:09 +0530

    added W3Resource basic part1 examples on 28032025

---
+++ src/javapractice/W3Resource/basic_part1/MedianOfArray.java
@@ -0,0 +1,24 @@
+package javapractice.W3Resource.basic_part1;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class MedianOfArray {
+    public static int getMedianValue(int[] array){
+        //check if array size is odd or even
+        if (array.length%2==0){
+            System.out.println("array length is even");
+            //if even, get two middle values and return the average
+            int result = (array[array.length/2-1]+array[array.length/2])/2;
+            return result;
+        }
+        //if odd return the middle element
+        System.out.println("array length is odd");
+        return array[array.length/2];
+    }
+    public static void main(String[] args) {
+        int[] array = {10, 2, 38, 22, 38, 23,21};
+        System.out.println("Orginal Array: "+ Arrays.toString(array));
+        System.out.println("Median of the said array of integers: "+getMedianValue(array));
+    }
+}
