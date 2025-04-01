34db276bc909ae379008f29082a5fe80d70698f2
VineelVarma N <vineelnemmani1training@gmail.com>
2025-04-01 20:16:34 +0530

    added W3Resource basic part1 examples on 01042025

---
+++ src/javapractice/W3Resource/basic_part1/RemoveDuplicatesInSortedArray.java
@@ -0,0 +1,28 @@
+package javapractice.W3Resource.basic_part1;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class RemoveDuplicatesInSortedArray {
+
+    public static HashSet<Integer> getUniqueSortedArray(int[] array){
+        //create hash set
+        HashSet<Integer> uniqueElements = new HashSet<>();
+        //get each element and add to set
+        for (int element: array){
+            uniqueElements.add(element);
+        }
+        System.out.println("The length of unique array : "+uniqueElements.size());
+        return uniqueElements;
+    }
+    public static void main(String[] args) {
+        int[] array = {1, 1, 2, 3, 3, 3, 4, 5, 6, 7, 7};
+        System.out.println("The orginal array : "+Arrays.toString(array));
+        System.out.println("The length of orginal array : "+array.length);
+        System.out.println("The unique array : "+getUniqueSortedArray(array));
+    }
+}
