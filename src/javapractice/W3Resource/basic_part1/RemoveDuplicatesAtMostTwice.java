0012403bfd4bcaed5379c573d90c1ba5abb50313
VineelVarma N <vineelnemmani1training@gmail.com>
2025-04-02 23:14:22 +0530

    added W3Resource basic part1 examples on 02042025

---
+++ src/javapractice/W3Resource/basic_part1/RemoveDuplicatesAtMostTwice.java
@@ -0,0 +1,25 @@
+package javapractice.W3Resource.basic_part1;
+
+public class RemoveDuplicatesAtMostTwice {
+
+    public static int getArrayWithTwoDuplicateValues(int[] numbers){
+        //check the array should not be empty
+        if (numbers==null || numbers.length==0){
+            return 0;
+        }
+        //declare count value as two
+        int index = 2;
+        for (int i = 2; i<numbers.length; i++){
+            //Allow duplicates at most two
+            if (numbers[i]!= numbers[index-2]) {
+                numbers[index]=numbers[i];
+                index++;
+            }
+        }
+        return index;
+    }
+    public static void main(String[] args) {
+        int[] numbers = {1, 1, 2, 3, 3, 3, 4, 5, 6, 7, 7, 7, 7};
+        System.out.println("The duplicate array length "+getArrayWithTwoDuplicateValues(numbers));
+    }
+}
--- src/javapractice/W3Resource/basic_part1/RemoveDuplicatesInSortedArray.java
+++ src/javapractice/W3Resource/basic_part1/RemoveDuplicatesInSortedArray.java
@@ -2,10 +2,6 @@ package javapractice.W3Resource.basic_part1;
 
 import java.util.Arrays;
 import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class RemoveDuplicatesInSortedArray {
 
