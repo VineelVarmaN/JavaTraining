771bb80f2c9c079fa900acb88b6e4414b7243a5e
VineelVarma N <vineelnemmani1training@gmail.com>
2025-05-03 23:09:11 +0530

    added hackerRank examples on 03052025

--- src/javalearning/myTraining/day3/records/EmployeeDetails.java
+++ src/javalearning/myTraining/day3/records/EmployeeDetails.java
@@ -2,7 +2,7 @@ package javalearning.myTraining.day3.records;
 
 /**
  * a record is a special type of class declaration for reducing the boilerplate code.
- * Java records were introduced with the intention to be used as a fast way to create data carrier classes to carry data
+ * Java records were introduced to be used as a fast way to create data carrier classes to carry data
  * between modules
  * we use POJO's (Plain Old Java Objects) and DTO's (Data Transfer Objects) to carry data between modules
  * instead of POJO class, we can use record classes.
