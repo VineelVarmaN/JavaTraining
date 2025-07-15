c187e2bec62ad6b3d58f2dbfbbc537396ab62c3c
VineelVarma N <vineelnemmani1training@gmail.com>
2025-07-16 00:13:12 +0530

    added TakeYourForward pattern examples on 16072025

---
+++ src/takeYouForward/patterns/AlphaTrianglePattern.java
@@ -0,0 +1,31 @@
+package takeYouForward.patterns;
+
+import java.util.Scanner;
+
+/**
+ * Sam is researching on Alpha-Triangles. So, he needs to create them for different integers ‘N’.
+ * An Alpha-Triangle is represented by the triangular pattern of alphabets in reverse order.
+ * For every value of ‘N’, help sam to print the corresponding Alpha-Triangle.
+ * Example:
+ * Input: ‘N’ = 3
+ * Output:
+ * C
+ * C B
+ * C B A
+ */
+public class AlphaTrianglePattern {
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = SCANNER.nextInt();
+        for (int i = 0; i<n; i++){
+            //to get thr final alphabet according to input
+            char ch = (char) ('A' + n-1);
+            //for alphabets
+            for (int j =0; j<=i; j++){
+                System.out.print(ch-- +" ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/takeYouForward/patterns/SymmetricVoidPattern.java
@@ -0,0 +1,55 @@
+package takeYouForward.patterns;
+
+import java.util.Scanner;
+
+/**
+ * Sam is curious about symmetric patterns, so he decided to create one.
+ * For every value of ‘N’, return the symmetry as shown in the example.
+ * Example:
+ * Input: ‘N’ = 3
+ * Output:
+ * * * * * * *
+ * * *     * *
+ * *         *
+ * *         *
+ * * *     * *
+ * * * * * * *
+ */
+public class SymmetricVoidPattern {
+    public static final Scanner SCANNER = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = SCANNER.nextInt();
+        //for top rows
+        for (int i = 0; i<n; i++){
+            //for stars
+            for (int j = 0; j<n-i; j++){
+                System.out.print("*"+" ");
+            }
+            //for spaces
+            for (int j = 0; j<2*i; j++){
+                System.out.print(" "+" ");
+            }
+            //for stars
+            for (int j = 0; j<n-i; j++){
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+        for (int i = n-1; i<n && i>=0; i--){
+            //for stars
+            for (int j = 0; j<n-i; j++){
+                System.out.print("*"+" ");
+            }
+            //for spaces
+            for (int j = 0; j<2*i; j++){
+                System.out.print(" "+" ");
+            }
+            //for stars
+            for (int j = 0; j<n-i; j++){
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+    }
+}
